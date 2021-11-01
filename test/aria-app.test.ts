a922f9dc2c2ac0660683fa865fe433f6702ed9a9
Tzvi-H <16guitar@gmail.com>
2021-11-01 11:32:45 -0500

    Initial commit

---
+++ test/aria-app.test.ts
@@ -0,0 +1,13 @@
+import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
+import * as cdk from '@aws-cdk/core';
+import * as AriaApp from '../lib/aria-app-stack';
+
+test('Empty Stack', () => {
+    const app = new cdk.App();
+    // WHEN
+    const stack = new AriaApp.AriaAppStack(app, 'MyTestStack');
+    // THEN
+    expectCDK(stack).to(matchTemplate({
+      "Resources": {}
+    }, MatchStyle.EXACT));
+});
